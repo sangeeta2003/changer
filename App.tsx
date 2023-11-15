c44667d0bb86b1d0d4e9a2d1f34f1be75f4b8eba
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-11-15 18:24:54 +0530

    m

--- App.tsx
+++ App.tsx
@@ -86,7 +86,7 @@ function App(): JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Monster
           </Section>
           <LearnMoreLinks />
         </View>
